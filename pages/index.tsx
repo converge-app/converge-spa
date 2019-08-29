fd1ed4794711f2b861b079bbe93bedca41b55ad5
samirhabibi <samir-habibi1@hotmail.com>
2019-08-29 11:45:59 +0200

    install redux on react and Material design UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
 function Home() {
-  return <div>Welcome to Next.js</div>;
+  return <div><Button>Hello</Button></div>;
+
 }
 
 export default Home;
