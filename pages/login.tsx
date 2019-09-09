cbe392538a9d968fd9cb5f88c702fcde299e79f8
samirhabibi <samir-habibi1@hotmail.com>
2019-09-09 15:17:32 +0200

    added login and signup page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,91 @@
 import React from "react";
 import Header from "../components/header";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 function Login(): JSX.Element {
+  const textfield = {
+    background: "#ffffff",
+    borderRadius: 10
+  };
+
+  const btn = {
+    marginTop: 20,
+    background: "#13A8FE"
+  };
+
   return (
-    <div className="header">
-      <Header></Header>
-      <p>Login page</p>
+    <div>
+      <div className="header">
+        <Header></Header>
+      </div>
+      <Container component="main" maxWidth="xs">
+        <div className="login">
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <form noValidate>
+            <div className="txtfield">
+              <TextField
+                style={textfield}
+                margin="normal"
+                variant="filled"
+                required
+                fullWidth
+                id="email"
+                label="Email Addresse"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                style={textfield}
+                variant="filled"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </div>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              style={btn}
+              color="primary"
+            >
+              Login
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="forgot_password" variant="body2">
+                  Glemte password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="signup" variant="body2">
+                  {"Har du ikke en konto? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+
+          <style jsx>{`
+            div.login {
+              margin-top: 100px;
+            }
+          `}</style>
+        </div>
+      </Container>
     </div>
   );
 }
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,9 +1,132 @@
 import React from "react";
+import Header from "../components/header";
+import {
+  Grid,
+  TextField,
+  Button,
+  Link,
+  Container,
+  Typography,
+  Box
+} from "@material-ui/core";
 
 function Signup(): JSX.Element {
+  const textfield = {
+    background: "#fff",
+    borderRadius: 4
+  };
+
+  const btn = {
+    marginTop: 20,
+    background: "#13A8FE"
+  };
+
   return (
     <div>
-      <p>Signup page</p>
+      <div className="header">
+        <Header></Header>
+      </div>
+
+      <Container component="main" maxWidth="xs">
+        <div className="formTag">
+          <Typography component="h1" variant="h5" style={{ marginBottom: 15 }}>
+            Sign up
+          </Typography>
+          <form noValidate>
+            <div className="txtfield">
+              <Grid container spacing={4}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    style={textfield}
+                    autoComplete="fname"
+                    name="firstName"
+                    variant="filled"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="Navn"
+                    autoFocus
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    style={textfield}
+                    variant="filled"
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Efternavn"
+                    name="lastname"
+                    autoComplete="lname"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    style={textfield}
+                    variant="filled"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Brugernavn"
+                    name="username"
+                    autoComplete="lname"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    style={textfield}
+                    variant="filled"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Addresse"
+                    name="email"
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    style={textfield}
+                    variant="filled"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                </Grid>
+              </Grid>
+            </div>
+
+            <div className="Button">
+              <Button
+                style={btn}
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+              >
+                Sign Up
+              </Button>
+            </div>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="login" variant="body2">
+                  Har du allerede en konto? Login
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+          <style jsx>{`
+            div.formTag {
+              margin-top: 100px;
+            }
+          `}</style>
+        </div>
+      </Container>
     </div>
   );
 }
