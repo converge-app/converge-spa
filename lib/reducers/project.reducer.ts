c67a4dbd9d7e5ec453206f269a626b5d5603f520
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2019-10-10 16:38:38 +0200

    Add project page (#28)

--- lib/reducers/project.reducer.ts
+++ lib/reducers/project.reducer.ts
@@ -1,62 +1,81 @@
-import {combineReducers} from 'redux';
-import {projectConstants} from '../constants/project.constants';
+import { combineReducers } from "redux";
+import { projectConstants } from "../constants/project.constants";
 
 const createProjectRequest = (state: any = {}, action: any) => {
-    switch (action.type) {
-        case projectConstants.CREATE_PROJECT_REQUEST:
-            return {
-                creatingProject: true,
-                project: action.project,
-            };
-        case projectConstants.CREATE_PROJECT_SUCCESS:
-            return {
-                createdProject: true,
-                project: action.project,
-            };
-        case projectConstants.CREATE_PROJECT_FAILURE:
-            return {};
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case projectConstants.CREATE_PROJECT_REQUEST:
+      return {
+        creatingProject: true,
+        project: action.project
+      };
+    case projectConstants.CREATE_PROJECT_SUCCESS:
+      return {
+        createdProject: true,
+        project: action.project
+      };
+    case projectConstants.CREATE_PROJECT_FAILURE:
+      return {};
+    default:
+      return state;
+  }
+};
 const getProjects = (state: any = {}, action: any) => {
-    switch (action.type) {
-        case projectConstants.GET_PROJECTS_REQUEST:
-            return {
-                gettingProjects: true,
-            }
-        case projectConstants.GET_PROJECTS_SUCCESS:
-            return {
-                gotProjects: true,
-                projects: action.projects,
-            }
-        case projectConstants.GET_PROJECTS_FAILURE:
-            return {}
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case projectConstants.GET_PROJECTS_REQUEST:
+      return {
+        gettingProjects: true
+      };
+    case projectConstants.GET_PROJECTS_SUCCESS:
+      return {
+        gotProjects: true,
+        projects: action.projects
+      };
+    case projectConstants.GET_PROJECTS_FAILURE:
+      return {};
+    default:
+      return state;
+  }
+};
 const getProject = (state: any = {}, action: any) => {
-    switch (action.type) {
-        case projectConstants.GET_PROJECT_REQUEST:
-            return {
-                gettingProject: true,
-                projectId: action.projectId,
-            }
-        case projectConstants.GET_PROJECT_SUCCESS:
-            return {
-                gotProjects: true,
-                project: action.project,
-            }
-        case projectConstants.GET_PROJECT_FAILURE:
-            return {}
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case projectConstants.GET_PROJECT_REQUEST:
+      return {
+        gettingProject: true,
+        projectId: action.projectId
+      };
+    case projectConstants.GET_PROJECT_SUCCESS:
+      return {
+        gotProjects: true,
+        project: action.project
+      };
+    case projectConstants.GET_PROJECT_FAILURE:
+      return {};
+    default:
+      return state;
+  }
+};
+
+const getOpenProjects = (state: any = {}, action: any) => {
+  switch (action.type) {
+    case projectConstants.GET_OPEN_PROJECTS_REQUEST:
+      return {
+        gettingOpenProjects: true
+      };
+    case projectConstants.GET_OPEN_PROJECTS_SUCCESS:
+      return {
+        gotOpenProjects: true,
+        projects: action.projects
+      };
+    case projectConstants.GET_OPEN_PROJECTS_FAILURE:
+      return {};
+    default:
+      return state;
+  }
+};
 
 export const project = combineReducers({
-    createProjectRequest,
-    getProjects,
-    getProject,
-})
+  createProjectRequest,
+  getProjects,
+  getProject,
+  getOpenProjects
+});
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,11 +1,26 @@
-import {NextPage} from 'next';
-import React from 'react';
-import DashboardLayout from '../../components/layouts/dashboard.layout';
+import { NextPage } from "next";
+import React, { useEffect } from "react";
+import FindOpenProjectsContent from "../../components/content/project/find-open-projects/find-open.projects.content";
+import DashboardLayout from "../../components/layouts/dashboard.layout";
+import { useDispatch, useSelector } from "react-redux";
+import { ProjectActions } from "../../lib/actions/project.actions";
+import { IProject } from "../../lib/models/project.model";
 
 const ProjectsPage: NextPage = () => {
-    return <DashboardLayout>
-        <div>This is projects page</div>
-    </DashboardLayout>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(ProjectActions.get());
+  }, []);
+
+  const projects: IProject[] = useSelector(
+    (state: any) => state.project.getProjects.projects
+  );
+
+  return (
+    <DashboardLayout>
+      <FindOpenProjectsContent projects={projects}></FindOpenProjectsContent>
+    </DashboardLayout>
+  );
 };
 
 export default ProjectsPage;
