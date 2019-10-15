6c0ea6abddc4ce14f0ed3406664cf740988f43e2
hermansen <hermansendev@gmail.com>
2019-10-15 20:50:06 +0200

    Add collaboration

--- lib/actions/collaboration.actions.ts
+++ lib/actions/collaboration.actions.ts
@@ -1,20 +1,65 @@
-import { CollaborationService } from "../../services/collaboration.service";
-import { IEvent } from "../models/collaboration.model";
-import { collaborationConstants } from "../constants/collaboration.constants";
+import { services } from '../../services';
+import { CollaborationService } from '../../services/collaboration.service';
+import { collaborationConstants } from '../constants/collaboration.constants';
+import { IEvent } from '../models/event.model';
 
 export class CollaborationActions {
+  public static send(message: string, projectId: string, type: string): any {
+    const request = (event: IEvent) => ({
+      type: collaborationConstants.COLLABORATION_CREATE_REQUEST,
+      event,
+    });
+    const success = (collaboration: IEvent[]) => ({
+      type: collaborationConstants.COLLABORATION_CREATE_SUCCESS,
+      collaboration,
+    });
+    const failure = (error: any) => ({
+      type: collaborationConstants.COLLABORATION_CREATE_FAILURE,
+      error,
+    });
+
+    let event: IEvent | null = null;
+
+    switch (type) {
+      case 'message':
+        event = {
+          projectId,
+          type,
+          ownerId: services.authentication.getId(),
+          content: JSON.stringify({
+            message,
+          }),
+        };
+    }
+
+    return async (dispatch: any) => {
+      if (event != null) {
+        dispatch(request(event));
+
+        try {
+          const response = await CollaborationService.create(event);
+          const eventRes = await response.data;
+
+          dispatch(success(eventRes));
+          dispatch(this.getByProjectId(projectId));
+        } catch (error) {
+          dispatch(failure(error));
+        }
+      }
+    };
+  }
   public static getByProjectId(projectId: string) {
     const request = (projectId: string) => ({
       type: collaborationConstants.GET_COLLABORATION_BY_PROJECT_REQUEST,
-      projectId
+      projectId,
     });
     const success = (collaboration: IEvent[]) => ({
       type: collaborationConstants.GET_COLLABORATION_BY_PROJECT_SUCCESS,
-      collaboration
+      collaboration,
     });
     const failure = (error: any) => ({
       type: collaborationConstants.GET_COLLABORATION_BY_PROJECT_FAILURE,
-      error
+      error,
     });
 
     return async (dispatch: any) => {
@@ -23,6 +68,7 @@ export class CollaborationActions {
       try {
         const response = await CollaborationService.getByProjectId(projectId);
         const collaboration: IEvent[] = await response.data;
+        console.log(collaboration);
         dispatch(success(collaboration));
       } catch (e) {
         dispatch(failure(e));
--- lib/models/event.model.ts
+++ lib/models/event.model.ts
@@ -1,7 +1,8 @@
 export interface IEvent {
   id?: string;
-  projectId?: string;
+  projectId: string;
+  ownerId: string;
   type: string;
   content: string;
-  timestamp: number;
+  timestamp?: number;
 }
