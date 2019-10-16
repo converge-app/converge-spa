89e72103c4bcfe55cff9d464756d52cf5855761c
hermansen <hermansendev@gmail.com>
2019-10-16 22:55:53 +0200

    Upload virkst

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,38 @@
-import {CssBaseline} from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import App from 'next/app';
 import Head from 'next/head';
 import React from 'react';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux';
 import theme from '../components/styles/theme/theme';
-import withReduxStore from '../lib/store/with-redux-store'
+import withReduxStore from '../lib/store/with-redux-store';
 
 class RootApp extends App {
-    public componentDidMount() {
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles && jssStyles.parentNode) {
-            jssStyles.parentNode.removeChild(jssStyles);
-        }
+  public componentDidMount() {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
     }
+  }
 
-    public render() {
-        // @ts-ignore
-        const {Component, pageProps, reduxStore} = this.props;
+  public render() {
+    // @ts-ignore
+    const { Component, pageProps, reduxStore } = this.props;
 
-        return (
-            <React.Fragment>
-                <Head>
-                    <title>Converge</title>
-                </Head>
-                <ThemeProvider theme={theme}>
-                    <CssBaseline/>
-                    <Provider store={reduxStore}>
-                        <Component {...pageProps} />
-                    </Provider>
-                </ThemeProvider>
-            </React.Fragment>
-        );
-    }
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Converge</title>
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Provider store={reduxStore}>
+            <Component {...pageProps} />
+          </Provider>
+        </ThemeProvider>
+      </React.Fragment>
+    );
+  }
 }
 
-export default withReduxStore(RootApp)
+export default withReduxStore(RootApp);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,34 +1,34 @@
-import {ServerStyleSheets} from '@material-ui/styles';
+import { ServerStyleSheets } from '@material-ui/styles';
 import Document from 'next/document';
 import React from 'react';
-import {HtmlRoot} from '../components/layouts/html-root';
+import { HtmlRoot } from '../components/layouts/html-root';
 
 class RootDocument extends Document {
-    public render() {
-        return <HtmlRoot/>;
-    }
+  public render() {
+    return <HtmlRoot />;
+  }
 }
 
-RootDocument.getInitialProps = async ctx => {
-    const sheets = new ServerStyleSheets();
-    const originalRenderPage = ctx.renderPage;
+RootDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
 
-    ctx.renderPage = () =>
-        originalRenderPage({
-            enhanceApp: App => props => sheets.collect(<App {...props} />),
-        });
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
 
-    const initialProps = await Document.getInitialProps(ctx);
+  const initialProps = await Document.getInitialProps(ctx);
 
-    return {
-        ...initialProps,
-        styles: [
-            <React.Fragment key='styles'>
-                {initialProps.styles}
-                {sheets.getStyleElement()}
-            </React.Fragment>,
-        ],
-    };
+  return {
+    ...initialProps,
+    styles: [
+      <React.Fragment key='styles'>
+        {initialProps.styles}
+        {sheets.getStyleElement()}
+      </React.Fragment>,
+    ],
+  };
 };
 
 export default RootDocument;
