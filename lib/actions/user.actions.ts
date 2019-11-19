68d29b8112313a9ef5b82aef8d85326cb75a2481
kjuulh <hermansendev@gmail.com>
2019-11-19 19:02:02 +0100

    Fix double banner for login

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,24 +1,21 @@
 import Router from 'next/router';
 import { services } from '../../services';
 import { userConstants } from '../constants/user.constants';
-import { alertActions } from './alert.actions';
 import { SubmitActions } from './submit.actions';
 
 const login = (email: string, password: string) => {
   return (dispatch: any) => {
-    dispatch(request({ email }));
     dispatch(SubmitActions.setSubmitting(true));
+    dispatch(request({ email }));
 
     services.authentication.login(email, password).then(
       (user: any) => {
         dispatch(success(user));
-        dispatch(alertActions.success('Logged in'));
         dispatch(SubmitActions.wasSuccess('Logged in'));
         Router.replace('/dashboard');
       },
       (error: any) => {
         dispatch(failure(error));
-        dispatch(alertActions.error(error.message));
         dispatch(SubmitActions.wasFailure('Failed to log in'));
       },
     );
@@ -66,24 +63,20 @@ const signUp = (
   return (dispatch: any) => {
     if (password !== confirmPassword) {
       dispatch(failure("Passwords didn't match"));
-      dispatch(alertActions.error("Passwords didn't match"));
       dispatch(SubmitActions.wasFailure("Passwords don't match"));
       return;
     }
-
-    dispatch(request({ email }));
     dispatch(SubmitActions.setSubmitting(true));
+    dispatch(request({ email }));
 
     services.authentication.signUp(email, password, firstName, lastName).then(
       (user: any) => {
         dispatch(success(user));
-        dispatch(alertActions.success('Sign up successful!'));
         dispatch(SubmitActions.wasSuccess('Sign up successful'));
         Router.replace('/login');
       },
       (error: any) => {
         dispatch(failure(error));
-        dispatch(alertActions.error(error.message));
         dispatch(SubmitActions.wasFailure("Couldn't sign up"));
       },
     );
